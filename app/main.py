b8e47eb606dc19dbc6c6527ba227abed97ee186f
hjkl888 <kai.chr160@gmail.com>
2024-02-17 23:48:26 +0700

    feat: sample call service with dependencies injection

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
+from app.repositories.random import RandomRepository
+from app.services.calculator import CalculatorService
 
 app = FastAPI()
+rand_repo = RandomRepository()
+cal_srv = CalculatorService(rand_repo)
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    number = str(cal_srv.random_with_constant(2021))
+    return {"Hello": "World " + number }
 
 
 @app.get("/items/{item_id}")
